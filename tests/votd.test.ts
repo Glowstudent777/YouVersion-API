66a09cb409b6fe1f16a7e8c1346b2a0ee5d7fa67
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-10-25 16:46:03 -0500

    Tests

--- tests/votd.test.ts
+++ tests/votd.test.ts
@@ -3,9 +3,12 @@ import { expect, it, describe } from 'vitest';
 
 describe("getVotd", () => {
     it("VOTD", async () => {
-        const verse = await getVotd();
+        const verse = await getVotd("en");
+        const verse2 = await getVotd("coffee");
 
         expect(verse?.citation).toBeDefined();
         expect(verse?.passage).toBeDefined();
+
+        expect(verse2).toBeUndefined();
     });
 });
