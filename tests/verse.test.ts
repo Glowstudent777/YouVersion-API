6bc2e5fc92caeb41ba41c78908fef8f8f40d4010
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-12-31 19:52:42 -0600

    Tests

--- tests/verse.test.ts
+++ tests/verse.test.ts
@@ -1,33 +1,39 @@
-import { getVerse } from "../src/api/v1/functions/verse";
-import { expect, it, describe } from 'vitest';
+import { getVerse } from "../src/api/v1/core/functions/verse";
+import { expect, it, describe } from "vitest";
 
 describe("getVerse", () => {
-    it("John 3:16", async () => {
-        const verse = await getVerse("John", "3", "16", "NIV");
+  it("John 3:16", async () => {
+    const verse = await getVerse("John", "3", "16", "NIV");
 
-        expect(verse?.citation).toBe("John 3:16");
-        expect(verse?.passage).toBe("For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
-    });
+    expect(verse?.citation).toBe("John 3:16");
+    expect(verse?.passage).toBe(
+      "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."
+    );
+  });
 
-    it("Genesis 1:1", async () => {
-        const verse = await getVerse("GEN", "1", "1", "KJV");
+  it("Genesis 1:1", async () => {
+    const verse = await getVerse("GEN", "1", "1", "KJV");
 
-        expect(verse?.citation).toBe("Genesis 1:1");
-        expect(verse?.passage).toBe("In the beginning God created the heaven and the earth.");
-    });
+    expect(verse?.citation).toBe("Genesis 1:1");
+    expect(verse?.passage).toBe(
+      "In the beginning God created the heaven and the earth."
+    );
+  });
 
-    it("Invalid verse", async () => {
-        const verse = await getVerse("JHN", "3", "54", "NIV");
+  it("Invalid verse", async () => {
+    const verse = await getVerse("JHN", "3", "54", "NIV");
 
-        expect(verse?.code).toBe(400);
-        expect(verse?.message).toBe("Verse not found");
-    });
+    expect(verse?.code).toBe(400);
+    expect(verse?.message).toBe("Verse not found");
+  });
 
-    it("Invalid book", async () => {
-        const book = "Coffee";
-        const verse = await getVerse(book, "5", "11", "NIV");
+  it("Invalid book", async () => {
+    const book = "Coffee";
+    const verse = await getVerse(book, "5", "11", "NIV");
 
-        expect(verse?.code).toBe(400);
-        expect(verse?.message).toBe(`Could not find book '${book}' by name or alias.`);
-    });
+    expect(verse?.code).toBe(400);
+    expect(verse?.message).toBe(
+      `Could not find book '${book}' by name or alias.`
+    );
+  });
 });
--- tests/votd.test.ts
+++ tests/votd.test.ts
@@ -1,14 +1,14 @@
-import { getVotd } from "../src/api/v1/functions/votd";
-import { expect, it, describe } from 'vitest';
+import { getVotd } from "../src/api/v1/core/functions/votd";
+import { expect, it, describe } from "vitest";
 
 describe("getVotd", () => {
-    it("VOTD", async () => {
-        const verse = await getVotd("en");
-        const verse2 = await getVotd("coffee");
+  it("VOTD", async () => {
+    const verse = await getVotd("en");
+    const verse2 = await getVotd("coffee");
 
-        expect(verse?.citation).toBeDefined();
-        expect(verse?.passage).toBeDefined();
+    expect(verse?.citation).toBeDefined();
+    expect(verse?.passage).toBeDefined();
 
-        expect(verse2).toBeUndefined();
-    });
+    expect(verse2).toBeUndefined();
+  });
 });
